a345d9f4ea59a0836d1c1f1a37e7ea5d40c122ac
ryuji <yrejgnl11@163.com>
2023-05-23 10:48:39 +0800

    feat: add SvgIcon component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import SvgIcon from '@/components/SvgIcon/index.vue'
 import { Plus } from '@element-plus/icons-vue'
 </script>
 
 <template>
   <div>
     <el-button type="primary" :icon="Plus">+</el-button>
+    <svg-icon name="phone" />
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //@ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'virtual:svg-icons-register'
+
 
 import App from './App.vue'
 
